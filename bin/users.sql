7f3b7f35cac2d423d865019d3db25b86473d7fb9
Aaron Prisk <aaronprisk@gmail.com>
2017-02-14 12:44:34 -0500

    Update users.sql
    Added missing selector

--- bin/users.sql
+++ bin/users.sql
@@ -1,1 +1,2 @@
+use login;
 INSERT INTO users (user_name, user_password_hash, user_email) VALUES("lagertha", "$2y$10$t/ah4VLdqLHnPghrwhXO/.UVKme9jsg0nPrLY1Up6JOJgPorzF5Sy", "admin@localhost.local");
